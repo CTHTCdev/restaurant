231701203cca16267613e3cd6ed31f64f08c167e
Marken <nctuan.ityu@gmail.com>
2023-01-27 00:36:56 +0000

    Split view., Masonry view

--- agent/lib/src/screens/home.dart
+++ agent/lib/src/screens/home.dart
@@ -3,6 +3,7 @@ import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:split_view/split_view.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -21,30 +22,72 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   Widget _bodyBuilder() {
-    return SafeArea(
-      minimum: EdgeInsets.all(15),
-      child: MasonryGridView.count(
-        crossAxisCount: (MediaQuery.of(context).size.width ~/ 100),
-        mainAxisSpacing: 15,
-        crossAxisSpacing: 15,
-        itemCount: 10,
-        itemBuilder: (context, index) {
-          return _buttonBuilder(text: 'Menu\n-------\nItem');
-        },
-      ),
+    return SafeArea(child: _testBuilder());
+  }
+
+  Widget _testBuilder() {
+    return SplitView(
+      viewMode: SplitViewMode.Horizontal,
+      indicator: SplitIndicator(viewMode: SplitViewMode.Horizontal),
+      controller: SplitViewController(weights: [0.7]),
+      children: [
+        SplitView(
+          viewMode: SplitViewMode.Vertical,
+          indicator: SplitIndicator(viewMode: SplitViewMode.Vertical),
+          controller: SplitViewController(weights: [0.4]),
+          children: [_topLeftBuilder(), _bottomLeftBuilder()],
+        ),
+        Container(),
+      ],
+    );
+  }
+
+  // Widget _testBuilder() {
+  //   return Column(
+  //     children: [_topLeftBuilder(), _bottomLeftBuilder()],
+  //   );
+  // }
+
+  Widget _topLeftBuilder() {
+    return MasonryGridView.count(
+      crossAxisCount: (MediaQuery.of(context).size.width ~/
+          (MediaQuery.of(context).size.height / 5)),
+      mainAxisSpacing: 15,
+      crossAxisSpacing: 15,
+      itemCount: 20,
+      itemBuilder: (context, index) {
+        return _buttonBuilder(text: 'Category\n-------\nItem');
+      },
+    );
+  }
+
+  Widget _bottomLeftBuilder() {
+    return MasonryGridView.count(
+      crossAxisCount: (MediaQuery.of(context).size.width ~/
+          (MediaQuery.of(context).size.height / 8)),
+      mainAxisSpacing: 15,
+      crossAxisSpacing: 15,
+      itemCount: 20,
+      itemBuilder: (context, index) {
+        return _buttonBuilder(text: 'Menu\n-------\nItem');
+      },
     );
   }
 
   Widget _buttonBuilder({required String text}) {
     return NeumorphicButton(
+      padding: EdgeInsets.symmetric(vertical: 15),
       style: NeumorphicStyle(
-        shape: NeumorphicShape.flat,
+        shape: NeumorphicShape.concave,
         boxShape: NeumorphicBoxShape.roundRect(
           BorderRadius.circular(5),
         ),
       ),
       child: Center(child: Text(text)),
-      onPressed: () {},
+      onPressed: () {
+        print('width' + MediaQuery.of(context).size.width.toString());
+        print('height' + MediaQuery.of(context).size.height.toString());
+      },
     );
   }
 }
